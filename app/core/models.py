dbde52e758a38cf43a80d552bf6213f25653c0fc
Rustam Garayev <rustamgarayev@Rustams-MacBook-Pro.local>
2020-01-21 13:04:49 +0400

    Added list ingredient feature

--- app/core/models.py
+++ app/core/models.py
@@ -52,7 +52,20 @@ class Tag(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name='users'
+        related_name='tag_users'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient to be used in a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='ingredient_users'
     )
 
     def __str__(self):
